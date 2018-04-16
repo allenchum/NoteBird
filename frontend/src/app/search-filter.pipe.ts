1ab9c5688feff429697ae5a3aa5c300fbd628e08
Allen Chum <chumwai@gmail.com>
2018-04-16 21:55:55 +0800

    search by tags

--- frontend/src/app/search-filter.pipe.ts
+++ frontend/src/app/search-filter.pipe.ts
@@ -1,26 +1,33 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'searchFilter'
+  name: "searchFilter"
 })
 export class SearchFilterPipe implements PipeTransform {
-
-  transform(notes: any, term: any, selector:string): any {
-    if (term === undefined){
+  transform(notes: any, term: any, selector: string): any {
+    if (term === undefined) {
       return notes;
-    }else if(selector == "title"){
-      return notes.filter(function(note){
+    } else if (selector == "title") {
+      return notes.filter(function(note) {
         return note.note_title.toLowerCase().includes(term.toLowerCase());
-      })
-    }else if(selector == "author"){
-      return notes.filter(function(note){
+      });
+    } else if (selector == "author") {
+      return notes.filter(function(note) {
         return note.author.toLowerCase().includes(term.toLowerCase());
-      })
-    }else if(selector == "tags"){
-      return notes.filter(function(note){
-        return note.tags.toLowerCase().includes(term.toLowerCase());
-      })
+      });
+    } else if (selector == "tags") {
+      return notes.filter(function(note) {
+        let haveTag: boolean = false;
+        for (let i = 0; i < note.tags.length; i++) {
+          if (note.tags[i].toLowerCase().includes(term.toLowerCase())) {
+            haveTag = true;
+            i = note.tags.length;
+          } else {
+            haveTag = false;
+          }
+        }
+        return haveTag;
+      });
     }
   }
 }
